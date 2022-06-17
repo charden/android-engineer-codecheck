4bbd453f4686c09d4baa3bdd64d75fe7a59091bb
charden <licht1017@gmail.com>
2022-06-17 20:03:45 +0900

    style: 💄 snake caseの変数をcamel caseに変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/OneFragment.kt
@@ -59,7 +59,7 @@ class OneFragment : Fragment(R.layout.fragment_one) {
     }
 }
 
-val diff_util = object : DiffUtil.ItemCallback<Item>() {
+val diffUtil = object : DiffUtil.ItemCallback<Item>() {
     override fun areItemsTheSame(oldItem: Item, newItem: Item): Boolean {
         return oldItem.name == newItem.name
     }
@@ -72,7 +72,7 @@ val diff_util = object : DiffUtil.ItemCallback<Item>() {
 
 class CustomAdapter(
     private val itemClickListener: OnItemClickListener,
-) : ListAdapter<Item, CustomAdapter.ViewHolder>(diff_util) {
+) : ListAdapter<Item, CustomAdapter.ViewHolder>(diffUtil) {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
