07fc80fc0aacf222664c044d51ad6d6ea0a82c93
charden <licht1017@gmail.com>
2022-06-19 17:02:46 +0900

    test: 💍 ViewModelのテストを追加

---
+++ app/src/test/kotlin/jp/co/yumemi/android/codecheck/SearchViewModelTest.kt
@@ -0,0 +1,67 @@
+package jp.co.yumemi.android.codecheck
+
+import io.ktor.client.*
+import io.ktor.client.engine.mock.*
+import io.ktor.http.*
+import io.ktor.utils.io.*
+import jp.co.yumemi.android.codecheck.helper.JsonLoadHelper
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class SearchViewModelTest {
+
+    @Test
+    fun kotlinで検索したときに正常に返り値が得られること() {
+        val mockEngine = MockEngine {
+            respond(
+                content = ByteReadChannel(JsonLoadHelper.loadJsonAsString("repositories.json")),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+        val client = HttpClient(mockEngine)
+        val viewModel = SearchViewModel(client)
+        val item = listOf(
+            Item(
+                "JetBrains/kotlin",
+                "https://avatars.githubusercontent.com/u/878437?v=4",
+                "Kotlin",
+                41802,
+                41802,
+                5162,
+                147
+            ),
+            Item(
+                "hussien89aa/KotlinUdemy",
+                "https://avatars.githubusercontent.com/u/7304399?v=4",
+                "Kotlin",
+                1462,
+                1462,
+                4936,
+                13
+            )
+        )
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, item)
+        }
+    }
+
+    @Test
+    fun 空文字で検索したときに正常に返り値が得られること() {
+        val mockEngine = MockEngine {
+            respond(
+                content = ByteReadChannel(""),
+                status = HttpStatusCode.OK,
+                headers = headersOf(HttpHeaders.ContentType, "application/json")
+            )
+        }
+        val client = HttpClient(mockEngine)
+        val viewModel = SearchViewModel(client)
+        runBlocking {
+            val result = viewModel.searchResults("")
+            assertEquals(result, listOf<Item>())
+        }
+    }
+}
