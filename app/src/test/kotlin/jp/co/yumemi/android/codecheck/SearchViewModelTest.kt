3d4c7473d571cc02d4f01335d1d653f099795bf5
charden <licht1017@gmail.com>
2022-06-19 17:27:56 +0900

    test: 💍 ViewModelの条件別のテストを追加

--- app/src/test/kotlin/jp/co/yumemi/android/codecheck/SearchViewModelTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/codecheck/SearchViewModelTest.kt
@@ -21,32 +21,35 @@ class SearchViewModelTest {
         val client = HttpClient(mockEngine)
         return SearchViewModel(client)
     }
+
+    private val baseItems = listOf(
+        Item(
+            "JetBrains/kotlin",
+            "https://avatars.githubusercontent.com/u/878437?v=4",
+            "Kotlin",
+            41802,
+            41802,
+            5162,
+            147
+        ),
+        Item(
+            "hussien89aa/KotlinUdemy",
+            "https://avatars.githubusercontent.com/u/7304399?v=4",
+            "Kotlin",
+            1462,
+            1462,
+            4936,
+            13
+        )
+    )
+
     @Test
     fun kotlinで検索したときに正常に返り値が得られること() {
         val viewModel = getViewModel("kotlin.json")
-        val item = listOf(
-            Item(
-                "JetBrains/kotlin",
-                "https://avatars.githubusercontent.com/u/878437?v=4",
-                "Kotlin",
-                41802,
-                41802,
-                5162,
-                147
-            ),
-            Item(
-                "hussien89aa/KotlinUdemy",
-                "https://avatars.githubusercontent.com/u/7304399?v=4",
-                "Kotlin",
-                1462,
-                1462,
-                4936,
-                13
-            )
-        )
+
         runBlocking {
             val result = viewModel.searchResults("kotlin")
-            assertEquals(result, item)
+            assertEquals(result, baseItems)
         }
     }
 
@@ -57,8 +60,81 @@ class SearchViewModelTest {
             val result = viewModel.searchResults("")
             assertEquals(result, listOf<Item>())
         }
-        val client = HttpClient(mockEngine)
-        val viewModel = SearchViewModel(client)
+    }
+
+    @Test
+    fun fullnameがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeFullName.json")
+        val item = baseItems.map { item -> item.copy(name = "") }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, item)
+        }
+    }
+
+    @Test
+    fun ownerがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeOwner.json")
+        val items = baseItems.map { item -> item.copy(ownerIconUrl = "") }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun languageがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeLanguage.json")
+        val items = baseItems.map { item -> item.copy(language = "") }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun stargazersCountがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeStargazersCount.json")
+        val items = baseItems.map { item -> item.copy(stargazersCount = 0) }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun watchersCountがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeWatchersCount.json")
+        val items = baseItems.map { item -> item.copy(watchersCount = 0) }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun forksCountがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeForksCount.json")
+        val items = baseItems.map { item -> item.copy(forksCount = 0) }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun openIssuesCountがjsonに含まれない時にエラーにならないこと() {
+        val viewModel = getViewModel("excludeOpenIssuesCount.json")
+        val items = baseItems.map { item -> item.copy(openIssuesCount = 0) }
+        runBlocking {
+            val result = viewModel.searchResults("kotlin")
+            assertEquals(result, items)
+        }
+    }
+
+    @Test
+    fun jsonのItemが空だった場合にエラーにならないこと() {
+        val viewModel = getViewModel("emptyItem.json")
         runBlocking {
             val result = viewModel.searchResults("")
             assertEquals(result, listOf<Item>())
