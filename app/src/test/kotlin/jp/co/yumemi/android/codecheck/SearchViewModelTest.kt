204bd96f5a8c8c00ffee017764d27a6de3b6d546
charden <licht1017@gmail.com>
2022-06-19 23:35:22 +0900

    refactor: 💡 検索処理と表示用の処理を分離

--- app/src/test/kotlin/jp/co/yumemi/android/codecheck/SearchViewModelTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/codecheck/SearchViewModelTest.kt
@@ -2,55 +2,49 @@ package jp.co.yumemi.android.codecheck
 
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.*
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class SearchViewModelTest {
     private lateinit var viewModel: SearchViewModel
 
     @Before
-    fun setup() {
+    fun setUp() {
+        val dispatcher = UnconfinedTestDispatcher()
+        Dispatchers.setMain(dispatcher)
         val api = MockApi()
-        val dataSource = ItemRemoteDataSource(api, Dispatchers.IO)
+        val dataSource = ItemRemoteDataSource(api, dispatcher)
         val repository = ItemRepositoryImpl(dataSource)
         viewModel = SearchViewModel(repository)
     }
 
-    private val baseItems = listOf(
-        Item(
-            "JetBrains/kotlin",
-            "https://avatars.githubusercontent.com/u/878437?v=4",
-            "Kotlin",
-            41802,
-            41802,
-            5162,
-            147
-        ),
-        Item(
-            "hussien89aa/KotlinUdemy",
-            "https://avatars.githubusercontent.com/u/7304399?v=4",
-            "Kotlin",
-            1462,
-            1462,
-            4936,
-            13
-        )
-    )
-
-    @Test
-    fun kotlinで検索したときに正常に返り値が得られること() {
-        runBlocking {
-            val result = viewModel.searchResults("kotlin")
-            assertEquals(result, baseItems)
-        }
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
     }
 
     @Test
+    fun kotlinで検索したときに正常に返り値が得られること() {
+        runTest {
+            viewModel.search("kotlin")
+            assertEquals(viewModel.result.value, MockApi().baseItems)
+        }
+    }
+
+
+    @Test
     fun 空文字で検索したときに正常に返り値が得られること() {
-        runBlocking {
-            val result = viewModel.searchResults("")
-            assertEquals(result, listOf<Item>())
+        runTest {
+            viewModel.search("")
+            assertEquals(viewModel.result.value, emptyList<Item>())
         }
     }
 }
