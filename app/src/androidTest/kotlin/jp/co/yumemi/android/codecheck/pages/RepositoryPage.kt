cf2ed37fd5285171a10ee29544bc73d55ebd1a72
charden <licht1017@gmail.com>
2022-06-20 09:22:50 +0900

    test: 💍 HiltでUITest時もInjectできるように設定

--- app/src/androidTest/kotlin/jp/co/yumemi/android/codecheck/pages/RepositoryPage.kt
+++ app/src/androidTest/kotlin/jp/co/yumemi/android/codecheck/pages/RepositoryPage.kt
@@ -41,4 +41,48 @@ class RepositoryPage {
         imageView.check(matches(withText("Written in Kotlin")))
         return this
     }
+
+    /**
+     * star数が反映されていることのチェック
+     *
+     * @return RepositoryPage
+     */
+    fun assertStarsView(): RepositoryPage {
+        val imageView = Espresso.onView(ViewMatchers.withId(R.id.starsView))
+        imageView.check(matches(withText("41802 stars")))
+        return this
+    }
+
+    /**
+     * watcher数が反映されていることのチェック
+     *
+     * @return RepositoryPage
+     */
+    fun assertWatchersView(): RepositoryPage {
+        val imageView = Espresso.onView(ViewMatchers.withId(R.id.watchersView))
+        imageView.check(matches(withText("41802 watchers")))
+        return this
+    }
+
+    /**
+     * fork数が反映されていることのチェック
+     *
+     * @return RepositoryPage
+     */
+    fun assertForksView(): RepositoryPage {
+        val imageView = Espresso.onView(ViewMatchers.withId(R.id.forksView))
+        imageView.check(matches(withText("5162 forks")))
+        return this
+    }
+
+    /**
+     * open issue数が反映されていることのチェック
+     *
+     * @return RepositoryPage
+     */
+    fun assertOpenIssuesView(): RepositoryPage {
+        val imageView = Espresso.onView(ViewMatchers.withId(R.id.openIssuesView))
+        imageView.check(matches(withText("147 open issues")))
+        return this
+    }
 }
