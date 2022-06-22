5829da5d5c18dacb4685d96b28f1d204f2da67f3
charden <licht1017@gmail.com>
2022-06-23 02:05:31 +0900

    fix: 🐛 ErrorMessageをContextに依存しない形に変更

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/search/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/search/SearchFragment.kt
@@ -8,7 +8,6 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -93,7 +92,7 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
     private fun showError(view: View, throwable: Throwable) {
         hideLoading()
 
-        showSnackBar(view, ErrorMessage(throwable).getMessage(requireContext()))
+        showSnackBar(view, getString(ErrorMessage(throwable).getMessageId()))
     }
 
     private fun showLoading(view: View) {
